122414e9c12ab451710cd2573fd80be2b45bb7c8
Joseph Handley <handleyj@hotmail.com>
2019-06-11 18:33:56 -0500

    Issue 20

--- fileFunctions.js
+++ fileFunctions.js
@@ -13,9 +13,7 @@ const { rm, cp } = require("shelljs");
 const wpThemePostInstallerInfo = require("@devloco/react-scripts-wptheme-utils/postInstallerInfo");
 
 const _doNotEditFile = "../!DO_NOT_EDIT_THESE_FILES!.txt";
-let _doNotEditContent = `Instead, edit the files in the 'react-src/src' and 'react-src/public' folders.
-These files are overwritten by Webpack every time you make edits to the files in those folders.
-You will lose all changes made to these files when that happens.`;
+const _readyToDeployFile = "../!READY_TO_DEPLOY!.txt";
 
 const fileFunctions = {
     copyPublicFolder: function(paths) {
@@ -24,16 +22,51 @@ const fileFunctions = {
             filter: (file) => file !== paths.appHtml && file.indexOf("index.html") == -1 && file.indexOf(wpThemePostInstallerInfo.postInstallerName) == -1
         });
     },
-    copyToThemeFolder: function(paths) {
+    copyToThemeFolder: function(paths, forBuild) {
         const copyFrom = path.join(paths.appBuild, "/*");
         cp("-rf", copyFrom, "..");
-    },
-    cleanThemeFolder: function(cleanAll) {
-        rm("-rf", "../static");
 
-        if (cleanAll === true) {
+        if (forBuild === true) {
+            fileFunctions.cleanThemeFolder(paths, true);
+            fileFunctions.deleteDeployFolder(paths);
+
+            const themeName = require(paths.appPackageJson).name;
+            const deployFolder = path.join("..", themeName);
+            fs.ensureDirSync(deployFolder);
+
+            cp("-rf", copyFrom, deployFolder);
+        }
+    },
+    cleanThemeFolder: function(paths, forBuild) {
+        rm("-rf", path.join("..", "static"));
+
+        if (forBuild === true) {
             rm("-f", _doNotEditFile);
-            rm("-r", "../precache*");
+            rm("-f", _readyToDeployFile);
+            rm("-r", path.join("..", "precache*"));
+
+            const assetManifest = path.join("..", "asset-manifest*");
+            const favIconIco = path.join("..", "favicon.ico");
+            const indexPhp = path.join("..", "index.php");
+            const manifestJson = path.join("..", "manifest.json");
+            const screenShotPng = path.join("..", "screenshot.png");
+            const styleCss = path.join("..", "style.css");
+            const serviceWorker = path.join("..", "service-worker.js");
+
+            rm("-r", assetManifest);
+            rm("-r", favIconIco);
+            rm("-r", indexPhp);
+            rm("-r", manifestJson);
+            rm("-r", screenShotPng);
+            rm("-r", styleCss);
+            rm("-r", serviceWorker);
+        }
+    },
+    deleteDeployFolder: function(paths) {
+        const themeName = require(paths.appPackageJson).name;
+        const deployFolder = path.join("..", themeName);
+        if (fs.existsSync(deployFolder)) {
+            rm("-rf", deployFolder);
         }
     },
     setupCopyToThemeFolder: function(paths) {
@@ -47,9 +80,38 @@ const fileFunctions = {
         cp("-rf", favIconIco, "..");
     },
     writeDoNotEditFile: function() {
+        const readyToDeployFile = "../!READY_TO_DEPLOY!.txt";
+        fs.access(readyToDeployFile, fs.constants.F_OK, (err) => {
+            if (!err) {
+                rm("-f", _readyToDeployFile);
+            }
+        });
+
+        const doNotEditContent = `Instead, edit the files in the 'react-src/src' and 'react-src/public' folders.
+        These files are overwritten by Webpack every time you make edits to the files in those folders.
+        You will lose all changes made to these files when that happens.`;
+
         fs.access(_doNotEditFile, fs.constants.F_OK, (err) => {
             if (err) {
-                fs.writeFile(_doNotEditFile, _doNotEditContent, "utf8", (err) => {});
+                fs.writeFile(_doNotEditFile, doNotEditContent, "utf8", (err) => {});
+            }
+        });
+    },
+    writeReadyToDeployFile: function(paths) {
+        fs.access(_doNotEditFile, fs.constants.F_OK, (err) => {
+            if (!err) {
+                rm("-f", _doNotEditFile);
+            }
+        });
+
+        const themeName = require(paths.appPackageJson).name;
+        const readyToDeployContent = `The folder named "${themeName}" is ready to deploy to your production server.
+
+If you need to continue developing your theme, simply change to the "react-src" folder and type the command: npm run start
+`;
+        fs.access(_readyToDeployFile, fs.constants.F_OK, (err) => {
+            if (err) {
+                fs.writeFile(_readyToDeployFile, readyToDeployContent, "utf8", (err) => {});
             }
         });
     }
